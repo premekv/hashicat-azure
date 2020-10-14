ad8955ec1b6ee2830b90aa0268e6d496a9a555ee
premekv <premek.vala@gmail.com>
2020-10-14 15:38:12 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,8 +1,8 @@
 module "vnet" {
   source  = "app.terraform.io/Premek-training/vnet/azurerm"
   version = "2.3.0"
-  # insert required variables here
-  resource_group_name = "premekvala-workshop"
-  vnet_name = "premekvala-vnet"
 
+  address_space = [var.address_space]
+  resource_group_name = "${var.prefix}-workshop"
+  vnet_name = "${var.prefix}-vnet"
 }
