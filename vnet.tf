4ba8ce35710f33260db09077f28ce328f480fe7e
premekv <premek.vala@gmail.com>
2020-10-14 15:26:26 +0000

    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -1,42 +1,7 @@
 module "vnet" {
   source  = "app.terraform.io/Premek-training/vnet/azurerm"
   version = "2.3.0"
-  resource "azurerm_resource_group" "myresourcegroup" {
-  name     = "${var.prefix}-workshop"
-  location = var.location
-  
-  tags = {
-    environment = "Production"
-  }
-}
-provider "azurerm" {
-  features {}
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "West Europe"
-}
-
-module "vnet" {
-  source              = "Azure/vnet/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_service_endpoints = {
-    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
-    subnet3 = ["Microsoft.AzureActiveDirectory"]
-  }
-
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-
-  depends_on = [azurerm_resource_group.example]
-}
-
   # insert required variables here
+  resource_group_name = "premekvala-workshop"
+
 }
